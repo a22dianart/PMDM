98ec2fecdf3c97249f8213f6fec8050033bcb65e
Diana <a22dianart@iessanclemente.net>
2023-10-08 11:18:11 +0200

    solucionado problemas proxecto2

---
+++ UD01_2One_More_Wine/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UD01_2One_More_Wine"
+include(":app")
+ 
