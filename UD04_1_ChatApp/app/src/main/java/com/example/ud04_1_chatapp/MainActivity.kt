21867c8af46b2ea809fce62e21d8a01db2cfadaa
Diana <a22dianart@iessanclemente.net>
2023-10-26 13:58:30 +0200

    chat

--- UD04_1_ChatApp/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
+++ UD04_1_ChatApp/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ud04_1_chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)  //hai que seleccionar o toolbar de google
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
