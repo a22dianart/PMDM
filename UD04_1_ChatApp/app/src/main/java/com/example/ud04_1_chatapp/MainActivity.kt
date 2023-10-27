eca77a2824feee9c2c652c7095759f080c0da52b
Diana <a22dianart@iessanclemente.net>
2023-10-27 12:51:52 +0200

    27/10/2023

--- UD04_1_ChatApp/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
+++ UD04_1_ChatApp/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
@@ -4,22 +4,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavHostController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //PARA A BARRA DE ARRIBA
+        //Facer que a nosa barra de actividade sexa a barra principal
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)  //hai que seleccionar o toolbar de google
-        val navHostFragment= supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        toolbar.setupWithNavController(navHostFragment.navController)
         setSupportActionBar(toolbar)
+
+        //Host de navegación (grafo asociado)
+        val navHostFragment= supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //Configuracion da barra de navegacion
+        val builderApp = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builderApp.build()
+
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        //PARA A BARRA DE ABAIXO
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setupWithNavController(navController) //levanos a inbox se lle damos para atrass
+
+        //PANEL LATERAL
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        builderApp.setOpenableLayout(drawerLayout)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
