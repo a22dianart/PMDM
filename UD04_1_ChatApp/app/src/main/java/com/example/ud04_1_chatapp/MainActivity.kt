5c9115f4d721f23f45daa870e756b39bbf2b72bf
Diana <a22dianart@iessanclemente.net>
2023-10-30 13:46:50 +0100

    30/10/2023

--- UD04_1_ChatApp/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
+++ UD04_1_ChatApp/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
